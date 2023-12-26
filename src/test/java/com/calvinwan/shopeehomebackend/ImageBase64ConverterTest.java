08afe7263630ce01c279b2d9c53914e18caa0e29
CalvinWan0101 <calvinwan0101@gmail.com>
2023-12-26 19:37:09 +0800

    [Feature Addition] Finished test of ImageBase64Converter.

--- src/test/java/com/calvinwan/shopeehomebackend/ImageBase64ConverterTest.java
+++ src/test/java/com/calvinwan/shopeehomebackend/ImageBase64ConverterTest.java
@@ -8,10 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ImageBase64ConverterTest {
     @Test
-    public void convert_the_image_and_the_copy_image() throws IOException {
-        String result = ImageBase64Converter.imageToBase64("src/main/resources/img/test/test.JPG");
-        assertEquals(result, ImageBase64Converter.imageToBase64("src/main/resources/img/test/test_copy.JPG"));
+    public void convert_image_to_base64() throws IOException {
+        String expect = ImageBase64Converter.imageToBase64("src/main/resources/img/test/test.JPG");
+        String result = ImageBase64Converter.imageToBase64("src/main/resources/img/test/test_copy.JPG");
+        assertEquals(expect, result);
 //        ImageBase64Converter.base64ToImage(result, "src/main/resources/img/3_tissue/tissue_1_copy.webp");
+    }
+
+    @Test
+    public void convert_base64_to_image() throws IOException {
+        String expect = ImageBase64Converter.imageToBase64("src/main/resources/img/test/test.JPG");
+        ImageBase64Converter.base64ToImage(expect, "src/main/resources/img/test/test_program_copy.JPG");
+        String result = ImageBase64Converter.imageToBase64("src/main/resources/img/test/test_program_copy.JPG");
+        assertEquals(expect, result);
     }
 
 }
